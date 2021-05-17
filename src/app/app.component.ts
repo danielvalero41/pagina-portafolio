ba437b2c9703149d4edff7f116b578b631caf68d
danielvalero41 <daniellvalero41>
2021-05-17 15:14:45 -0400

    Primera version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pagina-portafolio';
+  img_perfil = "../assets/img/perfil.jpg"
+  programador:boolean = true
+  trabajador:boolean 
+  algo:boolean = true
+
+  ngOnInit(){    
+  }
+
+  ver_programador(){
+    this.programador = true
+    this.trabajador = false
+  }
+
+  ver_trabajador(){
+    this.programador = false
+    this.trabajador = true
+  }
+  
 }
